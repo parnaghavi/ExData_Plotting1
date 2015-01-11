f14cb25867630aa8e03a3d07ab4f39eb1f1b6ad7
parnaghavi <parnaghavi@yahoo.com>
2015-01-11 15:44:54 -0500

    Create plot_2.R

---
+++ plot_2.R
@@ -0,0 +1,19 @@
+ 
+2 # set working directory (change this to fit your needs) 
+3 setwd('~/Source Code/GitHub/Exploratory-Data-Analysis') 
+4  
+5 # make sure the plots folder exists 
+6 if (!file.exists('plots')) { 
+7   dir.create('plots') 
+8 } 
+9  
+10 # load data 
+11 source('scripts/get_and_clean_data.R') 
+12  
+13 # open device 
+14 png(filename='plots/plot2.png',width=480,height=480,units='px') 
+15  
+16 # plot data 
+17 plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power (kilowatts)', xlab='', type='l') 
+18  
+19 # close device 
